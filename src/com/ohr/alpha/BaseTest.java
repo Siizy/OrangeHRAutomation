9f62b59cfc5856fa33570b90a4e6f594e5026a29
ChandanGupta <ChandanGupta@INDP277LT.mshome.net>
2021-10-28 08:36:32 +0530

    Ready project committed

---
+++ src/com/ohr/alpha/BaseTest.java
@@ -0,0 +1,16 @@
+package com.ohr.alpha;
+
+public class BaseTest {
+
+	public static void main(String[] args) {
+
+		String name = null;
+
+		if (name == null) {
+			name = "Bindu";
+			System.out.println("Name is: " + name);
+		}
+
+	}
+
+}
